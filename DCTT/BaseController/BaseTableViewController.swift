ca0699283ceb31face056fb956d14928a4be6469
Light413 <18016373660@163.com>
2017-11-17 17:32:17 +0800

    ...

--- DCTT/BaseController/BaseTableViewController.swift
+++ DCTT/BaseController/BaseTableViewController.swift
@@ -17,13 +17,8 @@ class BaseTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
-        tableView.showsVerticalScrollIndicator = false
+        //tableView.showsVerticalScrollIndicator = false
         
     }
 
