bfcc82ad3fb5c3e4823a300ef12b1c56c8bbfe94
Light413 <18016373660@163.com>
2018-02-06 23:39:03 +0800

    1.add PubTypeCollectionReusableView 2. modify pubTypeVC

--- DCTT/PubSelectTypeController.swift
+++ DCTT/PubSelectTypeController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICollectionViewDataSource{
+class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
 
     override var prefersStatusBarHidden: Bool {
         get{
@@ -19,14 +19,13 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
     
     @IBOutlet weak var collectionview: UICollectionView!
    
-    var dataArray = [String]()
-    
+    var dataArray = [[String]]()
+    let _head_section_titles = ["动态","生活服务"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.alpha = 0.8
-        
+        //view.alpha = 0.8
         automaticallyAdjustsScrollViewInsets = false;
         
 //        let path = Bundle.main.path(forResource: "all_category_item", ofType: "plist")
@@ -34,7 +33,10 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
 //        if let arr = _arr {
 //            dataArray = dataArray + arr;
 //        }
-        dataArray = dataArray + ["新鲜事","朋友圈","提问","商家信息","吃喝玩乐","相亲交友","求职招聘","房租租售","拼车出行","发布闲置"]
+        dataArray = dataArray + [["新鲜事","朋友圈","提问","吐槽","搞笑段子","校园"],
+                                 ["商家店铺","吃喝玩乐","相亲交友","求职招聘","房租租售","拼车出行","发布闲置","数码电子"]
+        ]
+        
         // Do any additional setup after loading the view.
         _init()
         
@@ -52,24 +54,26 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
         collectionview.dataSource = self
         
         let offset:CGFloat = 10
-        let _width = (kCurrentScreenWidth - offset *  2 - 10) / 3.0
+        let _width = (kCurrentScreenWidth - offset *  2 - 30) / 3.0
         
         let _layout = UICollectionViewFlowLayout()
-        _layout.itemSize = CGSize (width: _width, height: _width * 0.7)
+        _layout.itemSize = CGSize (width: _width, height: 60)
         _layout.minimumInteritemSpacing = 2
         _layout.minimumLineSpacing = 10
-        _layout.scrollDirection = .vertical
-        
+        _layout.scrollDirection = .vertical        
         collectionview.collectionViewLayout = _layout;
         
-        //collectionview.register(UINib (nibName: "AllCategoryCell", bundle: nil), forCellWithReuseIdentifier: "AllCategoryCellReuseIdentifierId")
+        
         collectionview.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCellIdentifier")
         
-        collectionview.backgroundColor  = UIColor.white
+        collectionview.register(UINib (nibName: "PubTypeCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "PubTypeCollectionReusableViewIdentifier")
+        
         collectionview.showsHorizontalScrollIndicator = false
-        collectionview.showsVerticalScrollIndicator = true
+        collectionview.showsVerticalScrollIndicator = false
         collectionview.contentInset = UIEdgeInsetsMake(15, offset, 10, offset)
         
+        collectionview.layer.borderWidth = 1
+        collectionview.layer.borderColor = kTableviewBackgroundColor.cgColor //UIColor.lightGray.cgColor
     }
     
     
@@ -93,14 +97,18 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
     
     
     //MARK: - UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return _head_section_titles.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataArray.count
+        return dataArray[section].count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCellIdentifier", for: indexPath)
         
-        let str = dataArray[indexPath.row]
+        let str = dataArray[indexPath.section][indexPath.row]
         
         for _v in cell.contentView.subviews{
             if _v is UILabel {
@@ -112,8 +120,8 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
         cell.contentView.addSubview(_t)
         _t.text = str
         _t.textAlignment = .center
-        _t.font = UIFont.systemFont(ofSize: 15)
-        //_t.textColor = UIColor.darkGray
+        _t.font = UIFont.systemFont(ofSize: 14)
+        _t.textColor = UIColor.darkGray
         
         cell.layer.borderWidth = 1
         cell.layer.borderColor = UIColor (red: 232/255.0, green: 232/255.0, blue: 232/255.0, alpha: 1).cgColor
@@ -141,6 +149,28 @@ class PubSelectTypeController: BaseViewController ,UICollectionViewDelegate,UICo
             let nav = BaseNavigationController (rootViewController:vc)
             UIApplication.shared.keyWindow?.rootViewController?.present(nav, animated: true, completion: nil)
         }
+    }
+    
+    
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionElementKindSectionHeader {
+            let v = collectionview.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "PubTypeCollectionReusableViewIdentifier", for: indexPath) as! PubTypeCollectionReusableView
+            
+            v.backgroundColor = UIColor.white
+            v.title.text = _head_section_titles[indexPath.section]
+            
+            return v
+        }
+        
+        return UICollectionReusableView()
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize (width: collectionview.frame.width, height: section == 0 ? 30 : 50)
     }
     
     
---
+++ DCTT/PubTypeCollectionReusableView.swift
@@ -0,0 +1,19 @@
+//
+//  PubTypeCollectionReusableView.swift
+//  DCTT
+//
+//  Created by wyg on 2018/2/6.
+//  Copyright © 2018年 Light.W. All rights reserved.
+//
+
+import UIKit
+
+class PubTypeCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var title: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
